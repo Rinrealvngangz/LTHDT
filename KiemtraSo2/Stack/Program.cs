4929a33106415965dd6d2ec0fe74a0dd06fea58e
Tuan <Rinrealvngangz.com>
2020-04-22 14:00:16 +0700

    add stack and SoPhuc

---
+++ KiemtraSo2/Stack/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        public class Stack
+        {
+
+            int[] stack;
+            int p;
+
+            // Khoi tao stack
+            public Stack(int n)
+            {
+                stack = new int[n];
+                p = -1;
+
+            }
+
+            public static bool kiemtraStackRong(int p)
+            {
+
+                bool check = false;
+
+                if (p == -1)
+                {
+                    return check = true;
+                }
+                else
+                    return check;
+            }
+            public static bool kiemtraStackDay(Stack a)
+            {
+
+                bool check = true;
+
+                if (a.p >= a.stack.Length)
+                {
+                    return check;
+                }
+                else
+                    return check = false;
+            }
+            //check stack rong ? day
+            public void kiemtraStack(Stack a)
+            {
+
+                if (kiemtraStackRong(a.p) == true)
+                {
+                    Console.WriteLine("Stack Rong!");
+                }
+
+                else if (kiemtraStackDay(a) == true)
+                {
+                    Console.WriteLine("Stack Day!");
+                }
+
+            }
+            // them 1 phan tu
+            public void PushMotPhanTu(int pt)
+            {
+
+                p++;
+                stack[p] = pt;
+
+            }
+            //lay ra 1 phan tu
+            public void PopMotPhanTu(Stack a)
+            {
+                Stack b = new Stack(10);//stack chua kq 
+                int kq;
+                do
+                {
+                    kq = a.stack[a.p--];
+                    b.PushMotPhanTu(kq);
+
+                } while (a.p >= 0);
+                b.xuatStack();
+            }
+
+            public void thucThi(int so, int soCanDoi)
+            {
+                int sodu;
+                do
+                {
+                    sodu = so % soCanDoi;//1 ,0,1
+                    so = so / soCanDoi;//2,1,0
+                    PushMotPhanTu(sodu);
+                } while (so > 0);
+            }
+
+            public void doiCoSo(int so, Stack a, int soCanDoi)
+            {
+
+                thucThi(so, soCanDoi);
+                PopMotPhanTu(a);
+            }
+            public void xuatStack()
+            {
+                int i;
+                Console.Write("So sau khi doi: ");
+                for (i = 0; i <= p; i++)
+                {
+                    if (stack[i] == 10)
+                    {
+                        Console.Write("A");
+                    }
+                    else if (stack[i] == 11)
+                    {
+                        Console.Write("B");
+                    }
+                    else if (stack[i] == 12)
+                    {
+                        Console.Write("C");
+                    }
+                    else if (stack[i] == 13)
+                    {
+                        Console.Write("D");
+                    }
+                    else if (stack[i] == 14)
+                    {
+                        Console.Write("E");
+                    }
+                    else if (stack[i] == 15)
+                    {
+                        Console.Write("F");
+                    }
+                    else
+                        Console.Write(stack[i]);
+                }
+                Console.WriteLine();
+            }
+
+            public void xuatmenu()
+            {
+                Console.WriteLine("======Chuong Trinh Doi Co so=======");
+                Console.WriteLine("0:Phim 0 ket thuc chuong trinh");
+                Console.WriteLine("1:Doi Co so");
+            }
+            public int Chonmenu()
+            {
+
+                int stt;
+                bool kiemtra = false;
+
+                for (; ; )
+                {
+
+                    Console.WriteLine("Nhap chuc nang 1: ");
+                    do
+                    {
+                        kiemtra = Int32.TryParse(Console.ReadLine(), out stt);
+                        if (!kiemtra)
+                        {
+                            Console.Write("khong nhap chu  : ");
+                        }
+                    } while (!kiemtra);
+                    if (0 <= stt && stt <= 1)
+                        break;
+                }
+                return stt;
+            }
+            public void xulymenu(int somenu)
+            {
+
+                int soCanDoi;
+                bool kiemtra = false;
+                int so;
+                Stack a = new Stack(100);
+                switch (somenu)
+                {
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine(" ket thuc chuong trinh");
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+
+                        Console.Clear();
+                        a.kiemtraStack(a);
+                        Console.Write("Nhap so thap phan can doi : ");
+                        do
+                        {
+                            kiemtra = Int32.TryParse(Console.ReadLine(), out so);
+                            if (!kiemtra)
+                            {
+                                Console.Write("khong nhap chu  : ");
+                            }
+                        } while (!kiemtra);
+                        Console.Write("Nhap he so muon doi : ");
+                        do
+                        {
+                            kiemtra = Int32.TryParse(Console.ReadLine(), out soCanDoi);
+                            if (!kiemtra)
+                            {
+                                Console.Write("khong nhap chu  : ");
+                            }
+                        } while (!kiemtra);
+                        a.doiCoSo(so, a, soCanDoi);
+                        break;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int somenu;
+            Stack a = new Stack(100);
+
+            do
+            {
+                Console.Clear();
+                a.xuatmenu();
+                somenu = a.Chonmenu();
+                a.xulymenu(somenu);
+
+            } while (somenu > 0);
+            Console.ReadLine();
+        }
+    }
+    }
+
+
---
+++ Lab4/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        public class soPhuc
+        {
+            int a, b;
+            bool kiemtra = false;
+          
+
+
+            public void kiemTraPhanThuc()
+            {
+                do
+                {
+                    Console.WriteLine("Nhap lai so a:");
+                    kiemtra = Int32.TryParse(Console.ReadLine(), out a);
+
+                } while (!kiemtra);
+            }
+            public void kiemTraPhanAo()
+            {
+                do
+                {
+                    Console.WriteLine("Nhap lai so b:");
+                    kiemtra = Int32.TryParse(Console.ReadLine(), out b);
+
+                } while (!kiemtra);
+            }
+            public void nhapSoPhuc()
+            {
+                Console.WriteLine("Nhap so a:");
+                kiemtra = Int32.TryParse(Console.ReadLine(),out a);
+                if (!kiemtra) kiemTraPhanThuc();
+                Console.WriteLine("Nhap so b:");
+                kiemtra = Int32.TryParse(Console.ReadLine(), out b);
+                if (!kiemtra) kiemTraPhanAo();
+            }
+            public soPhuc tong(soPhuc ob1)
+            {
+                soPhuc ob = new soPhuc();
+                ob.a = a + ob1.a;
+                ob.b = b + ob1.b;
+                return ob;
+            }
+            public soPhuc tru(soPhuc ob1)
+            {
+                soPhuc ob = new soPhuc();
+                ob.a = a - ob1.a;
+                ob.b = (b - ob1.b);//(a-c) + (b-d)i
+                return ob;
+            }
+
+            public soPhuc nhan(soPhuc ob1)
+            {
+                soPhuc ob = new soPhuc();
+                ob.a = (a * ob1.a) - (b * ob1.b);
+                ob.b = (a *  ob1.b)+(b*ob1.b);
+                return ob;
+            }
+            //public soPhuc chia(soPhuc ob1)
+            //{
+            //    soPhuc ob = new soPhuc();
+            //    ob.a = ((a + b));
+            //    ob.b = (a * ob1.b) + (b * ob1.b);
+            //    return ob;
+            //}
+
+              public  void  kiemtra_ThuanAo_That(soPhuc ob)
+            {
+               
+                if (ob.b == 0)
+                {
+                    Console.WriteLine("So that");
+                   
+                }
+                else
+                    Console.WriteLine("So thuan ao");
+             
+            }
+       
+            public static bool operator ==(soPhuc ob, soPhuc ob1)
+            {
+                bool checkSoThuc = false;
+                if (ob.a == ob1.a && ob.b == ob1.b)
+                    return checkSoThuc =true;
+
+                else
+                    return checkSoThuc; 
+            }
+
+            public static bool operator !=(soPhuc ob, soPhuc ob1)
+            {
+                bool checkSoThuc = false;
+                if (ob.a != ob1.a && ob.b != ob1.b)
+                    return checkSoThuc = true;
+
+                else
+                    return checkSoThuc ;
+            }
+            public void xuatSoPhuc()
+            {
+              
+                Console.WriteLine("So phuc sau khi nhap:{0}+{1}i ", a, b);
+            }
+        }
+        static void Main(string[] args)
+        {
+            soPhuc ob3 = new soPhuc();
+            soPhuc ob = new soPhuc();
+            ob.nhapSoPhuc();
+            ob.xuatSoPhuc();
+            soPhuc ob2 = new soPhuc();
+            ob2.nhapSoPhuc();
+            ob2.xuatSoPhuc();
+            Console.WriteLine("Tong hai so phuc");
+            ob3 =ob.tong(ob2);
+           ob3.xuatSoPhuc();
+            Console.WriteLine("Hieu hai so phuc");
+            ob3 = ob.tru(ob2);
+            ob3.xuatSoPhuc();
+
+            Console.WriteLine("Nhan hai so phuc");
+            ob3 = ob.nhan(ob2);
+            ob3.xuatSoPhuc();
+
+            Console.WriteLine("kiem tra so phuc ob: ");
+            ob.kiemtra_ThuanAo_That(ob);
+         
+            Console.WriteLine("so phuc ob == so phuc ob2 ? {0}", ob == ob2);
+            Console.WriteLine("so phuc ob != so phuc ob2 ? {0}", ob != ob2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
